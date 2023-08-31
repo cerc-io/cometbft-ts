7a8031edd5fb0ab959d62cd9257a00059e433196
prathamesh0 <prathamesh.musale0@gmail.com>
2023-08-31 12:16:55 +0530

    Add methods to PrivValidator interface

--- packages/cometbft/src/types/priv-validator.ts
+++ packages/cometbft/src/types/priv-validator.ts
@@ -1,7 +1,18 @@
+import * as crypto from '../crypto/crypto';
+import * as cmtprototypes from '../proto/tendermint/types/types.pb';
+
 // PrivValidator defines the functionality of a local CometBFT validator
 // that signs votes and proposals, and never double signs.
 // TODO: Implement
-export interface PrivValidator {}
+export interface PrivValidator {
+  // TODO: Can throw an error
+  getPubKey (): crypto.PubKey;
+
+  // TODO: Can throw an error
+  signVote (chainID: string, vote: cmtprototypes.Vote): void
+  // TODO: Can throw an error
+  signProposal (chainID: string, proposal: cmtprototypes.Proposal): void
+}
 
 // TODO: Implement
 export class PrivValidatorsByAddress {
