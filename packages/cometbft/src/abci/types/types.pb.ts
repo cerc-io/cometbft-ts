c49aa46c9aaaa9bc8c9fdeb9881a742ad3914e1d
prathamesh0 <prathamesh.musale0@gmail.com>
2023-08-31 16:56:00 +0530

    Add methods to Mempool interface

--- packages/cometbft/src/abci/types/types.pb.ts
+++ packages/cometbft/src/abci/types/types.pb.ts
@@ -1,5 +1,11 @@
 // TODO: Generate
 
+export class Response {}
+
+export class ResponseCheckTx {}
+
+export class ResponseDeliverTx {}
+
 // Event allows application developers to attach additional information to
 // ResponseBeginBlock, ResponseEndBlock, ResponseCheckTx and ResponseDeliverTx.
 // Later, transactions may be queried using these events.
